decbe67d76e536b7862cf106df9a7d2d7998371f
doctor4t <doctor4t@gmail.com>
2023-11-23 16:09:48 +0100

    spin

--- src/main/java/doctor4t/astronomical/client/render/entity/block/AstralDisplayBlockEntityRenderer.java
+++ src/main/java/doctor4t/astronomical/client/render/entity/block/AstralDisplayBlockEntityRenderer.java
@@ -1,5 +1,6 @@
 package doctor4t.astronomical.client.render.entity.block;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.sammy.lodestone.setup.LodestoneParticles;
 import com.sammy.lodestone.setup.LodestoneRenderLayers;
@@ -10,7 +11,9 @@ import com.sammy.lodestone.systems.rendering.particle.ParticleBuilders;
 import doctor4t.astronomical.client.render.world.AstraWorldVFXBuilder;
 import doctor4t.astronomical.common.Astronomical;
 import doctor4t.astronomical.common.block.entity.AstralDisplayBlockEntity;
+import doctor4t.astronomical.common.init.ModParticles;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
@@ -29,13 +32,20 @@ public class AstralDisplayBlockEntityRenderer<T extends AstralDisplayBlockEntity
 
 	public void render(T entity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j) {
 		if (entity.getWorld() != null) {
-			float time = ((float) (MinecraftClient.getInstance().world.getTime() % 2400000L) + MinecraftClient.getInstance().getTickDelta())*5f;
+			float time = ((float) (MinecraftClient.getInstance().world.getTime() % 2400000L) + MinecraftClient.getInstance().getTickDelta());
 
 			matrixStack.push();
 			matrixStack.translate(.5f, .5f,.5f);
 
 
-			matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(time));
+			float speedModifier = 0.01f;
+			float value = time;
+			float selfRotation = -time *1f;
+
+			matrixStack.translate(Math.sin(value * speedModifier) * 10f, 0, Math.cos(value * speedModifier) * 10f);
+//			matrixStack.scale(20, 20, 20);
+
+			matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(selfRotation));
 
 			float SIZE = 2f;
 			RandomGenerator random = entity.getWorld().random;
@@ -54,14 +64,14 @@ public class AstralDisplayBlockEntityRenderer<T extends AstralDisplayBlockEntity
 					25);
 			matrixStack.pop();
 
-			ParticleBuilders.create(LodestoneParticles.WISP_PARTICLE)
-				.setScale(SIZE * (1 + random.nextFloat()))
-				.setAlpha(0, 0.2f, 0)
-				.setSpin((float) random.nextGaussian() / 100f)
-				.enableNoClip()
-				.setLifetime(20)
-				.setColor(1f, 1f, 1f)
-				.spawn(entity.getWorld(), x2, y2, z2);
+//			ParticleBuilders.create(ModParticles.STAR)
+//				.setScale(SIZE/1.5f * (1 + random.nextFloat()))
+//				.setAlpha(0, 0.2f, 0)
+//				.setSpin((float) random.nextGaussian() / 100f)
+//				.enableNoClip()
+//				.setLifetime(20)
+//				.setColor(1f, 1f, 1f)
+//				.spawn(entity.getWorld(), x2, y2, z2);
 		}
 	}
 }
